3d4348b6a0cb1acfb47b92f8471bf3c98e97ced5
watanabe-akihiro <k022g0118@g.neec.ac.jp>
2024-10-28 12:13:55 +0900

    モデル差し替え終了

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -83,31 +83,53 @@ public class PlayerScript : MonoBehaviour
         float speed = 0.0f;
 
 
-        //キャラクターが地面に接しているかを判定
-        if (this.characterController.isGrounded)
+        ////キャラクターが地面に接しているかを判定
+        //if (this.characterController.isGrounded)
+        //{
+        //    //パッドのスティック入力を所得して移動ベクトルを作成
+        //    vec = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        //    //入力値が0.1以上なら速さを設定
+        //    if (vec.magnitude > 0.1)
+        //    {
+        //        //スティックの倒し具合で速さを変更
+        //        speed = Mathf.Lerp(this.minSpeed, this.maxSpeed, vec.magnitude);
+
+        //        //向きの変更
+        //        Vector3 dir = vec.normalized;
+        //        float rotate = Mathf.Acos(dir.z);
+        //        if (dir.x < 0) rotate = -rotate;
+        //        rotate *= Mathf.Rad2Deg;
+        //        Quaternion Q = Quaternion.Euler(0, rotate, 0);
+
+        //        //モデルの向きを変更
+        //        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Q, this.turnRate);
+
+        //    }
+        //    //移動ベクトルを正規化
+        //    vec = vec.normalized;
+        //}
+
+        //パッドのスティック入力を所得して移動ベクトルを作成
+        vec = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        //入力値が0.1以上なら速さを設定
+        if (vec.magnitude > 0.1)
         {
-            //パッドのスティック入力を所得して移動ベクトルを作成
-            vec = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-            //入力値が0.1以上なら速さを設定
-            if (vec.magnitude > 0.1)
-            {
-                //スティックの倒し具合で速さを変更
-                speed = Mathf.Lerp(this.minSpeed, this.maxSpeed, vec.magnitude);
+            //スティックの倒し具合で速さを変更
+            speed = Mathf.Lerp(this.minSpeed, this.maxSpeed, vec.magnitude);
 
-                //向きの変更
-                Vector3 dir = vec.normalized;
-                float rotate = Mathf.Acos(dir.z);
-                if (dir.x < 0) rotate = -rotate;
-                rotate *= Mathf.Rad2Deg;
-                Quaternion Q = Quaternion.Euler(0, rotate, 0);
+            //向きの変更
+            Vector3 dir = vec.normalized;
+            float rotate = Mathf.Acos(dir.z);
+            if (dir.x < 0) rotate = -rotate;
+            rotate *= Mathf.Rad2Deg;
+            Quaternion Q = Quaternion.Euler(0, rotate, 0);
 
-                //モデルの向きを変更
-                this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Q, this.turnRate);
+            //モデルの向きを変更
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Q, this.turnRate);
 
-            }
-            //移動ベクトルを正規化
-            vec = vec.normalized;
         }
+        //移動ベクトルを正規化
+        vec = vec.normalized;
 
         //移動速度を設定
         this.velocity.x = vec.x * speed;
@@ -115,7 +137,7 @@ public class PlayerScript : MonoBehaviour
         this.velocity.z = vec.z * speed;
 
         //重力による落下を設定
-        this.velocity.y += Physics.gravity.y * Time.deltaTime;
+        //this.velocity.y += Physics.gravity.y * Time.deltaTime;
 
 
         if (!Input.GetButton("Fire3"))
