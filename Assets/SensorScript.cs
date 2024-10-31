27c4daa50057d4f2a98f557659b3b1f9cfafb338
watanabe-akihiro <k022g0118@g.neec.ac.jp>
2024-10-31 23:17:25 +0900

    アタッチするものが変わっていた

--- Assets/SensorScript.cs
+++ Assets/SensorScript.cs
@@ -25,7 +25,7 @@ public class SensorScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(111);
     }
 
     private void OnTriggerEnter(Collider other)
