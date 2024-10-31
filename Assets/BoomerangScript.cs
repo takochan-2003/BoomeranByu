a618d7e307429a6191a9d2c7318c40ec67671edb
watanabe-akihiro <k022g0118@g.neec.ac.jp>
2024-10-31 21:25:01 +0900

    競合の解決

--- Assets/BoomerangScript.cs
+++ Assets/BoomerangScript.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -29,7 +28,7 @@ public class BoomerangScript : MonoBehaviour
     //タイマー変数
     private int timer;
     //敵に追尾するかのフラグ
-    private bool homingFlag;
+    public bool homingFlag;
 
     private Vector3 velocity;
 
@@ -68,9 +67,7 @@ public class BoomerangScript : MonoBehaviour
 
             if(sensorScript.findTarget == false)
             {
-                transform.rotation = Quaternion.LookRotation((sensorScript.targetPosition - transform.position), Vector3.up);
-                transform.LookAt(sensorScript.targetPosition);
-                homingFlag = false;
+                transform.rotation = Quaternion.LookRotation((sensorScript.targetPosition - transform.position), Vector3.zero);
             }
         }
         else
@@ -78,7 +75,7 @@ public class BoomerangScript : MonoBehaviour
             //ブーメランが戻ってくるときに軌道をプレイヤーに向ける
             LookAt(player);
         }
-        Debug.Log(sensorScript.targetPosition);
+        Debug.Log(transform.position);
     }
 
     //ブーメランが戻ってくるときに軌道をプレイヤーに向ける関数
@@ -126,7 +123,6 @@ public class BoomerangScript : MonoBehaviour
         backPower = minBackPower;
         backFlag = false;
         timer = 0;
-        homingFlag = true;
     }
 
     private void OnTriggerEnter(Collider other)
--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -9,7 +9,7 @@ public class GameManagerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemy, new Vector3(0, 0, 8), Quaternion.identity);
+        Instantiate(enemy, new Vector3(0, -1, 8), Quaternion.identity);
     }
 
     // Update is called once per frame
